1bd8ab38c7972f410bf2b9e967cc5929522aea25
sugar <deix@yandex.ru>
2022-10-28 23:45:31 +0300

    integration with external service

--- app/src/main/java/com/example/fatfoods/MainActivity.java
+++ app/src/main/java/com/example/fatfoods/MainActivity.java
@@ -1,50 +1,56 @@
 package com.example.fatfoods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Hashtable;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
-    Hashtable<String, Integer> foods = new Hashtable<String, Integer>();
-    AutoCompleteTextView autoCompleteTextView;
-    EditText input;
+    Hashtable<String, Integer> hashTableFoods = new Hashtable<String, Integer>();
+    EditText inputGram;
     TextView inputResult;
-    String selectedItem;
+    TextView inputFood;
+    String selectedFood;
+    int setGram;
     ArrayAdapter<String> adapterItems;
+    OkHttpClient client = new OkHttpClient();
+    private static final String BASE_URL_SERVICE = "https://csrusader.tk/search-food";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        foods.put("Apple", 52);
-        foods.put("Banana", 89);
-        foods.put("Cucumber", 15);
-        autoCompleteTextView = findViewById(R.id.food_types);
-        input = findViewById(R.id.count);
-        inputResult = findViewById(R.id.result);
-        adapterItems = new ArrayAdapter<>(this, R.layout.list_items, Collections.list(foods.keys()));
-        autoCompleteTextView.setAdapter(adapterItems);
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                selectedItem = adapterView.getItemAtPosition(i).toString();
-                Toast.makeText(MainActivity.this, selectedItem, Toast.LENGTH_SHORT).show();
-            }
-        });
-        input.addTextChangedListener(new TextWatcher() {
+        inputGram = findViewById(R.id.idGram);
+        inputResult = findViewById(R.id.idResult);
+        inputFood = findViewById(R.id.idFood);
+        inputGram.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -57,14 +63,114 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
+                setGram = 0;
                 if (editable.length() > 0) {
-                    Integer inputValue = Integer.parseInt(editable.toString());
-                    Integer foodValue = foods.get(selectedItem);
-                    if (foodValue != null) {
-                        inputResult.setText(String.valueOf((double)inputValue * ((double)foodValue / 100)));
-                    }
+                    setGram = Integer.parseInt(editable.toString());
                 }
+                calcCalories();
             }
         });
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.dialog_searchable_spinner);
+        dialog.getWindow().setLayout(650, 800);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        inputFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+                EditText dialogEditText = dialog.findViewById(R.id.idQueryFood);
+                ListView dialogListView = dialog.findViewById(R.id.idListFoods);
+                adapterItems = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, Collections.list(hashTableFoods.keys()));
+                dialogListView.setAdapter(adapterItems);
+                dialogEditText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+                        if (editable.length() >= 2) {
+                            searchFood(editable.toString());
+                        }
+                    }
+                });
+                dialogListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        selectedFood = adapterItems.getItem(i);
+                        inputFood.setText(selectedFood);
+                        dialog.dismiss();
+                        calcCalories();
+                    }
+                });
+            }
+        });
+    }
+
+    private void searchFood(String query) {
+        Request request = new Request.Builder()
+                .url(BASE_URL_SERVICE + "?query=" + query)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        hashTableFoods.clear();
+                        adapterItems.clear();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        hashTableFoods.clear();
+                        adapterItems.clear();
+                        JSONArray foods = new JSONArray();
+                        try {
+                            String data = response.body().string();
+                            System.out.print(data);
+                            foods = new JSONArray(data);
+                        } catch (JSONException | IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (foods.length() == 0) {
+                            return;
+                        }
+                        for (int j = 0; j < foods.length(); j++) {
+                            try {
+                                JSONObject food = foods.getJSONObject(j);
+                                hashTableFoods.put(food.optString("name"), food.optInt("calories"));
+                            } catch (JSONException e) {
+                                return;
+                            }
+                        }
+                        adapterItems.addAll(Collections.list(hashTableFoods.keys()));
+                        adapterItems.getFilter().filter(query);
+                    }
+                });
+            }
+        });
+    }
+
+    private void calcCalories() {
+        Integer foodValue = hashTableFoods.get(selectedFood);
+        if (foodValue != null) {
+            inputResult.setText(
+                String.valueOf(
+                    Math.round((double)setGram * ((double)foodValue / 100))
+                )
+            );
+        }
     }
 }
